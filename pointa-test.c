d119055aa6e7f5dc2816ed9fef0951cd8a47e16d
Ryo Suzuki <ryo.s12330@gmail.com>
2023-02-05 18:59:33 +0900

    update pointa

--- pointa-test.c
+++ pointa-test.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void change_array(int* data);
+void print_array(int data[]);
+
 int main(void)
 {
     int *p;
@@ -17,5 +20,24 @@ int main(void)
     printf("p = %i\n", *p);
     printf("i = %i\n", i);
 
+    // int array[10];
+    int array[10] = { 15, 78, 98, 15, 98, 85, 17, 35, 42, 15 };
+
+    change_array(array);
+    print_array(array);
+
     return 0;
+}
+
+void change_array(int* data)
+{
+    data[0] = 0;
+    data[1] = 0;
+}
+
+void print_array(int data[])
+{
+    for (int i = 0; i < 10; i++) {
+        printf("%p:%d\n", &data[i], data[i]);
+    }
 }
